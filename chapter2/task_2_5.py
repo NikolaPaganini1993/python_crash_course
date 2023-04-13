3c2c46020eb906be504cd1c4585b67b69431b2ca
«Nikola» <plotnikov@allremax.ru>
2023-04-13 16:44:17 +0800

    Added Chapter2

---
+++ chapter2/task_2_5.py
@@ -0,0 +1,5 @@
+# 2.5. Знаменитая цитата: найдите известное высказывание, которое вам понравилось.
+# Выведите текст цитаты с именем автора. Результат должен выглядеть примерно так (включая кавычки):
+# Albert Einstein once said, "A person who never made a mistake never tried anything new."
+message = 'Albert Einstein once said, "A person who never made a mistake never tried anything new."'
+print(message)
---
+++ chapter2/task_2_6.py
@@ -0,0 +1,6 @@
+# 2.6. Знаменитая цитата 2: повторите упражнение 2.5, но на этот раз сохраните имя автора
+# цитаты в переменной famous_person. Затем составьте сообщение и сохраните его в новой
+# переменной с именем message. Выведите свое сообщение.
+name = "Albert Einstein"
+message = '"A person who never made a mistake never tried anything new."'
+print(f'{name} once said: {message}')
